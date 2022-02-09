86ba75ecceee94f5495c79a245d5f2acb380e728
edddjunior <edsonbergamojunior@gmail.com>
2022-02-09 16:52:52 -0300

    corrected typo

--- src/main/java/com/southsystem/ApiVoting/app/resources/requests/CreateVotingAgendaRequestDTO.java
+++ src/main/java/com/southsystem/ApiVoting/app/resources/requests/CreateVotingAgendaRequestDTO.java
@@ -20,7 +20,6 @@ public class CreateVotingAgendaRequestDTO implements Serializable {
 	private static final long serialVersionUID = -3436276456948550079L;
 
 	private Long id;
-
 	@NotEmpty(message = "You must provide a title")
 	@Length(min = 5, max = 80, message = "Invalid length. 5 to 80 characters.")
 	private String title;
