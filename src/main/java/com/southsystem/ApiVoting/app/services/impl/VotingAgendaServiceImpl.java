906c1d747c0f0a1e9ee94368191f4861b3deb42f
edddjunior <edsonbergamojunior@gmail.com>
2022-02-09 17:38:37 -0300

    added insertion methods with @Transactional

---
+++ src/main/java/com/southsystem/ApiVoting/app/services/impl/VotingAgendaServiceImpl.java
@@ -0,0 +1,37 @@
+package com.southsystem.ApiVoting.app.services.impl;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.southsystem.ApiVoting.app.domain.entities.UserEntity;
+import com.southsystem.ApiVoting.app.domain.entities.VotingAgendaEntity;
+import com.southsystem.ApiVoting.app.domain.repositories.VotingAgendaRepository;
+import com.southsystem.ApiVoting.app.services.VotingAgendaService;
+
+@Service
+public class VotingAgendaServiceImpl implements VotingAgendaService {
+
+	@Autowired
+	private VotingAgendaRepository votingAgendaRepository;
+
+	@Override
+	public Optional<VotingAgendaEntity> findVotingAgenda(Long votingAgendaId) {
+		return votingAgendaRepository.findById(votingAgendaId);
+	}
+
+	@Override
+	@Transactional
+	public VotingAgendaEntity createVotingAgenda(VotingAgendaEntity votingAgenda) {
+		return votingAgendaRepository.save(votingAgenda);
+	}
+
+	@Override
+	public VotingAgendaEntity addUserToVotingAgenda(VotingAgendaEntity votingAgenda, UserEntity user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
